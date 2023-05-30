9efd903d77a5544830183f3b53c3a26baea02e96
Ismael R <ismael_br7@hotmail.com>
2023-05-30 02:04:14 -0600

    [Spotify component] Sidebar components

---
+++ components/Sidebar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React, { useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+
+interface ISidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<ISidebarProps> = ({ children }) => {
+  const pathName = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathName !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathName === "/search",
+        href: "/search",
+      },
+    ],
+    [pathName]
+  );
+
+  return (
+    <div className='flex h-full '>
+      <div
+        className='
+        hidden
+        md:flex
+        flex-col
+        gap-y-2
+        bg-black
+        h-full
+        w-[300px]
+        p-2
+      '>
+        <Box>
+          <div className='flex flex-col px-5 py-4 gap-y-4'>
+            {routes.map(route => (
+              <SidebarItem key={route.label} {...route} />
+            ))}
+          </div>
+        </Box>
+        <Box className='h-full overflow-y-auto'>Song library</Box>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
